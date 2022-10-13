786659ce1fd9632369dccd4d2575b8f672a87a04
Dan Paxton <danpaxton@Mac-Air.local>
2022-10-13 04:46:16 -0400

    new build

--- client/src/App.js
+++ client/src/App.js
@@ -27,7 +27,7 @@ const theme = createTheme({
 });
 
 export const api = axios.create({
-  baseURL: process.env.BASE_URL ?? "http://localhost:5000"
+  baseURL: process.env.BASE_URL
 })
 
 export const App = () => {
